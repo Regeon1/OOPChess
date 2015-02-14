285b4f3685da4fef35fb32a9541357bcd38d6736
josany1 <josany@utu.fi>
2015-02-14 13:49:33 +0200

    Added turns

--- Versus.java
+++ Versus.java
@@ -4,38 +4,59 @@
 public class Versus extends Gamemode implements PieceProperties{
     
     public Boolean movePiece(Vector2 from, Vector2 to){
-        //Check wheter the move is valid
-        if(0 > from.x || from.x > 7 || 0 > from.y || from.y > 7 ||
-                0 > to.x || to.x > 7 || 0 > to.y || to.y > 7 )
-            return false;
-        //Check wheter the move is legal
+    	//White, Black
+    	private enum turn = PieceProperties.Color.WHITE; 
+    	
+    	if(turn==board[from.x][from.y].getColor()){
+    		
+        	//Check wheter the move is valid
+            if(0 > from.x || from.x > 7 || 0 > from.y || from.y > 7 ||
+                    0 > to.x || to.x > 7 || 0 > to.y || to.y > 7 )
+                return false;
+            //Check wheter the move is legal
 
-        if(board[from.x][from.y].checkMove(from, to, board)){
-            //Attack
-            if(board[to.x][to.y].getColor()==PieceProperties.Color.WHITE)
-        	lostWhite.add(board[to.x][to.y]);
+            if(board[from.x][from.y].checkMove(from, to, board)){
+                //Attack
+                if(board[to.x][to.y].getColor()==PieceProperties.Color.WHITE)
+            	lostWhite.add(board[to.x][to.y]);
 
-            if(board[to.x][to.y].getColor()==PieceProperties.Color.BLACK)
-        	lostBlack.add(board[to.x][to.y]);
+                if(board[to.x][to.y].getColor()==PieceProperties.Color.BLACK)
+            	lostBlack.add(board[to.x][to.y]);
 
 
-            //Identify castling
-            if(board[from.x][from.y].getType() == Type.KING && 
-                    Vector2.length(from) > 2){
-                //Move rook
-                System.out.println("Moving rook!");
-                board[to.x == 6 ? 5:3][to.y] = board[to.x == 6 ? 7:0][to.y];
-                board[to.x == 6 ? 7:0][to.y] = new EmptyPiece();
-                //Normal move below moves the king
+                //Identify castling
+                if(board[from.x][from.y].getType() == Type.KING && 
+                        Vector2.length(from) > 2){
+                    //Move rook
+                    System.out.println("Moving rook!");
+                    board[to.x == 6 ? 5:3][to.y] = board[to.x == 6 ? 7:0][to.y];
+                    board[to.x == 6 ? 7:0][to.y] = new EmptyPiece();
+                    //Normal move below moves the king
+                }
+                
+                //Normal move
+                board[to.x][to.y] = board[from.x][from.y];
+                board[from.x][from.y] = new EmptyPiece();
+                
+                //Changing turns
+        		if(turn=PieceProperties.Color.WHITE){
+        			turn=PieceProperties.Color.BLACK;
+        		}
+        		if(turn=PieceProperties.Color.BLACK){
+        			turn=PieceProperties.Color.WHITE;
+        		}
+
+            }else{
+                System.out.println("Error: Illegal move!");
             }
-            
-            //Normal move
-            board[to.x][to.y] = board[from.x][from.y];
-            board[from.x][from.y] = new EmptyPiece();
-
-        }else{
-            System.out.println("Error: Illegal move!");
-        }
-        return false;
+            return false;
+    		
+    		
+      	}
+    	else{
+    		return false;
+    	}
+    	
+    	
     }
 }
